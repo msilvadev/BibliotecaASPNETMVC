be3e56efb48bd211628c9e09984f7ccc85623e6c
matheus souza <theus_ss@hotmail.com>
2018-03-18 01:08:51 -0300

    commit inicial

---
+++ BibliotecaASP/BibliotecaASP/Controllers/LivroController.cs
@@ -0,0 +1,96 @@
+﻿using BibliotecaASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BibliotecaASP.Controllers
+{
+    public class LivroController : Controller
+    {
+        // GET: Livro
+        public ActionResult Index()
+        {
+            List<Livro> Livros = new List<Livro>();
+            Livros.Add(new Livro()
+                {
+                    Id = 05,
+                    Nome = "Meu Primeiro Livro"
+                });
+            return View(Livros);
+        }
+
+        // GET: Livro/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Livro/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Livro/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Livro/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Livro/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Livro/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Livro/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
