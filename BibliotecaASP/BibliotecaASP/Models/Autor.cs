8c4a1ad1dd3e716767736f899cc3e996d93f6f82
matheus souza <theus_ss@hotmail.com>
2018-03-18 11:38:40 -0300

    Finalizado crate livro

---
+++ BibliotecaASP/BibliotecaASP/Models/Autor.cs
@@ -0,0 +1,9 @@
+﻿namespace BibliotecaASP.Models
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
---
+++ BibliotecaASP/BibliotecaASP/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace BibliotecaASP.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
